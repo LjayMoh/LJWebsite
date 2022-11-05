6c3d6c054ba5db47bc2a89411e2b9a996d0e9877
LjayMoh <2194907@students.wits.ac.za>
2022-11-06 00:55:25 +0200

    Update
    Changes for D3.js

--- art.js
+++ art.js
@@ -1,4 +1,164 @@
-const rect = document.createElement('rect');
-rect.setAttribute('width', 500);
-rect.setAttribute('height', 450);
-svg.appendChild(rect);
+const design = d3.select(".design");
+
+const svg = design.append("svg")
+    .attr("width", 900)
+    .attr("height", 700);
+
+svg
+  .append("circle")
+  .attr("cx", 110)
+  .attr("cy", 260)
+  .attr("r", 55)
+  .attr("fill", "white");
+   
+svg
+  .append("rect")
+  .attr("width", 60)
+  .attr("height", 60)
+  .attr("x", 80)
+  .attr("y", 230)
+  .attr("fill", "lightblue");
+
+svg
+  .append("circle")
+  .attr("cx", 200)
+  .attr("cy", 200)
+  .attr("r", 65)
+  .attr("fill", "aqua");
+   
+svg
+  .append("rect")
+  .attr("width", 70)
+  .attr("height", 70)
+  .attr("x", 165)
+  .attr("y", 170)
+  .attr("fill", "black");
+
+svg
+  .append("circle")
+  .attr("cx", 490)
+  .attr("cy", 250)
+  .attr("r", 120)
+  .attr("fill", "aqua");
+   
+svg
+  .append("rect")
+  .attr("width", 60)
+  .attr("height", 60)
+  .attr("x", 500)
+  .attr("y", 330)
+  .attr("fill", "black");
+
+svg
+  .append("circle")
+  .attr("cx", 300)
+  .attr("cy", 300)
+  .attr("r", 90)
+  .attr("fill", "aqua");
+   
+svg
+  .append("rect")
+  .attr("width", 95)
+  .attr("height", 95)
+  .attr("x", 257)
+  .attr("y", 262)
+  .attr("fill", "white");
+
+svg
+  .append("circle")
+  .attr("cx", 360)
+  .attr("cy", 360)
+  .attr("r", 40)
+  .attr("fill", "lightblue");
+   
+svg
+  .append("rect")
+  .attr("width", 120)
+  .attr("height", 120)
+  .attr("x", 430)
+  .attr("y", 190)
+  .attr("fill", "pink");
+
+svg
+  .append("circle")
+  .attr("cx", 715)
+  .attr("cy", 390)
+  .attr("r", 150)
+  .attr("fill", "lightblue");
+
+svg
+  .append("rect")
+  .attr("width", 160)
+  .attr("height", 160)
+  .attr("x", 635)
+  .attr("y", 310)
+  .attr("fill", "aqua");
+
+svg
+  .append("text")
+  .text("LIVE")
+  .attr("fill", "black")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 85)
+  .attr("y", 265);
+
+svg
+  .append("text")
+  .text("YOUR")
+  .attr("fill", "white")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 170)
+  .attr("y", 215);
+
+svg
+  .append("text")
+  .text("LIFE...")
+  .attr("fill", "black")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 270)
+  .attr("y", 320);
+
+svg
+  .append("text")
+  .text("Alive")
+  .attr("fill", "black")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 335)
+  .attr("y", 365);
+
+svg
+  .append("text")
+  .text("Or")
+  .attr("fill", "black")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 470)
+  .attr("y", 250);
+
+  svg
+  .append("text")
+  .text("Just")
+  .attr("fill", "white")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 510)
+  .attr("y", 365);
+
+svg
+  .append("text")
+  .text("Breathing?")
+  .attr("fill", "black")
+  .attr("font-size", 20)
+  .attr("font-weight", "bold")
+  .attr("x", 665)
+  .attr("y", 390);
+  
+  
+  
+  
+  
+  
