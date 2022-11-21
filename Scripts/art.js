f1447c0012b91fe24d64867151a9d47244084c7a
LjayMoh <2194907@students.wits.ac.za>
2022-11-21 16:14:01 +0200

    Changes and Updates
    New changes

--- Scripts/art.js
+++ Scripts/art.js
@@ -1,161 +1,117 @@
-const design = d3.select(".design");
+/***Exam Data Art ***/
 
-const svg = design.append("svg")
-    .attr("width", 900)
-    .attr("height", 700);
+let movieDataLink = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
 
-svg
-  .append("circle")
-  .attr("cx", 110)
-  .attr("cy", 260)
-  .attr("r", 55)
-  .attr("fill", "white");
+let movieData 
+
+let canvas = d3.select('#myCanvas')
+let tooltip = d3.select('#tooltip')
+
+let drawTreeMap = () => {
+
+  let hierarchy = d3.hierarchy(movieData, (node) => {
+      return node['children']
+  })
+  .sum((node) => {
+      return node['value']
+  })
+  .sort((node1, node2) => {
+      return node2['value'] - node1['value']
+  })
+
+  let createTreeMap = d3.treemap()
+                        .size([1000, 900])
+
+  createTreeMap(hierarchy)
+
+  let movieSlates = hierarchy.leaves()
+  console.log(movieSlates)
+
+  let block = canvas
+          .selectAll('g')
+          .data(movieSlates)
+          .enter()
+          .append('g')
+          .attr('transform', (movie) => {
+            return 'translate(' + movie['x0'] + ', ' + movie['y0'] + ')'
+          })
+
+  block
+    .append('rect')
+    .attr('class', 'tile')
+    .attr('fill', (movie) => {
+      let category = movie['data']['category']
+      if (category === 'Action') {
+        return 'lightblue'
+      }else if (category === 'Drama') {
+        return 'brown'
+      }else if (category === 'Adventure') {
+        return 'orange'
+      }else if (category === 'Family') {
+        return 'khaki'
+      }else if (category === 'Comedy') {
+        return 'tan'
+      }else if (category === 'Animation') {
+        return 'pink'
+      }else if (category === 'Biography') {
+        return 'green'
+      }
+    }).attr('data-name', (movie) => {
+       return movie['data']['name']
+    }).attr('data-category', (movie) => {
+       return movie['data']['category']
+    })
+    .attr('data-value', (movie) => {
+        return movie['data']['value']
+    })
+    .attr('width', (movie) => {
+       return movie['x1'] - movie['x0']
+    })
+    .attr('height', (movie) => {
+       return movie['y1'] - movie['y0'] 
+    })
+    .on('mouseover', (movie) => {
+      tooltip.transition()
+             .style('visibility', 'visible')
+
+      let value = movie['data']
+
+      tooltip.html (
+        '$ ' + value + '-'
+      )
+
+      tooltip.attr('data-value', movie['data'])
    
-svg
-  .append("rect")
-  .attr("width", 60)
-  .attr("height", 60)
-  .attr("x", 80)
-  .attr("y", 230)
-  .attr("fill", "lightblue");
+    })
+    .on('mouseout', (movie) => {
+      tooltip.transition()
+              .style('visibility', 'hidden')
+    })
+    
 
-svg
-  .append("circle")
-  .attr("cx", 200)
-  .attr("cy", 200)
-  .attr("r", 65)
-  .attr("fill", "aqua");
-   
-svg
-  .append("rect")
-  .attr("width", 70)
-  .attr("height", 70)
-  .attr("x", 165)
-  .attr("y", 170)
-  .attr("fill", "black");
+  block
+    .append('text')
+    .text((movie) => {
+       return movie['data']['name']
+    })
+    .attr('x', 5)
+    .attr('y', 20)
+}
 
-svg
-  .append("circle")
-  .attr("cx", 490)
-  .attr("cy", 250)
-  .attr("r", 120)
-  .attr("fill", "aqua");
-   
-svg
-  .append("rect")
-  .attr("width", 60)
-  .attr("height", 60)
-  .attr("x", 500)
-  .attr("y", 330)
-  .attr("fill", "black");
+d3.json(movieDataLink).then(
+  (data, error) => {
+    if(error) {
+      console.log(error)
+    } else {
+      movieData = data
+      console.log(movieData)
+      drawTreeMap()
+    }
+  }
+)
 
-svg
-  .append("circle")
-  .attr("cx", 300)
-  .attr("cy", 300)
-  .attr("r", 90)
-  .attr("fill", "aqua");
-   
-svg
-  .append("rect")
-  .attr("width", 95)
-  .attr("height", 95)
-  .attr("x", 257)
-  .attr("y", 262)
-  .attr("fill", "white");
 
-svg
-  .append("circle")
-  .attr("cx", 360)
-  .attr("cy", 360)
-  .attr("r", 40)
-  .attr("fill", "lightblue");
-   
-svg
-  .append("rect")
-  .attr("width", 120)
-  .attr("height", 120)
-  .attr("x", 430)
-  .attr("y", 190)
-  .attr("fill", "pink");
 
-svg
-  .append("circle")
-  .attr("cx", 715)
-  .attr("cy", 390)
-  .attr("r", 150)
-  .attr("fill", "lightblue");
-
-svg
-  .append("rect")
-  .attr("width", 160)
-  .attr("height", 160)
-  .attr("x", 635)
-  .attr("y", 310)
-  .attr("fill", "aqua");
-
-svg
-  .append("text")
-  .text("LIVE")
-  .attr("fill", "black")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 85)
-  .attr("y", 265);
-
-svg
-  .append("text")
-  .text("YOUR")
-  .attr("fill", "white")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 170)
-  .attr("y", 215);
-
-svg
-  .append("text")
-  .text("LIFE...")
-  .attr("fill", "black")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 270)
-  .attr("y", 320);
-
-svg
-  .append("text")
-  .text("Alive")
-  .attr("fill", "black")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 335)
-  .attr("y", 365);
-
-svg
-  .append("text")
-  .text("Or")
-  .attr("fill", "black")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 470)
-  .attr("y", 250);
-
-  svg
-  .append("text")
-  .text("Just")
-  .attr("fill", "white")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 510)
-  .attr("y", 365);
-
-svg
-  .append("text")
-  .text("Breathing?")
-  .attr("fill", "black")
-  .attr("font-size", 20)
-  .attr("font-weight", "bold")
-  .attr("x", 665)
-  .attr("y", 390);
   
   
   
